0a186131611234e2f61143c31e112e61c6438acd
santhoshkumark2003 <santhoshk13022003@gmail.com>
2025-12-20 23:03:55 +0530

    url updated

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -79,7 +79,7 @@ export const forgotPassword = async (req, res) => {
       user.email,
       "Password Reset link",
       `You are receiving this email because you have requested to reset your password.
-       Please click the following link to reset your password: http://localhost:5173/reset-password/${user._id}/${token}
+       Please click the following link to reset your password: https://frontend-rho-virid-92.vercel.app/reset-password/${user._id}/${token}
        If you did not request this, please ignore this email.`
     );
     res.status(200).json({ message: "Email sent successfully" });
